9205bab4dc370d4bad8958a6ed864167c1589e2c
sudarshan441 <sudarshanpujari812@gmail.com>
2023-01-03 10:22:01 +0530

    added

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-
+ 
 server.use(middlewares)
-server.use(router)
-server.listen(8001, () => {
+server.use('', router)
+server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running')
 })
